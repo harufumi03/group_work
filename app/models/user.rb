5304258ec9b1f43c17df8273b45bf2a1cfd692fc
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-11-02 17:59:14 +0900

    ユーザ登録機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true 
+  before_validation { email.downcase! }
+  has_secure_password
+  
+end
